fd18be8cb3b60a46b712da2e9957a0b3741699a9
Miriam <miriamkrebs606@gmail.com>
2018-03-13 11:59:50 +0100

    Work done
    done

---
+++ Google Books API/Assets/Scripts/inputScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputScript : MonoBehaviour
+{
+    private string result;
+    private GameObject API;
+    // Use this for initialization
+    void Start()
+    {
+        API = GameObject.Find("API");
+        var textInput = GetComponent<InputField>();
+        var se = new InputField.SubmitEvent();
+        textInput.onEndEdit.AddListener(SubmitName);
+    }
+
+    public void SubmitName(string arg0)
+    {
+        // call method from API
+        API.GetComponent<GoogleBooksAPI>().doSearch(arg0);
+
+    }
+}
