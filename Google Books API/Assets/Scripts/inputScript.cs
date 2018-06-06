c0467d4e41dd67ec7e973f64e44fbea86feaad05
jakexdxd <jakexdxd@sol.dk>
2018-06-06 18:25:18 +0200

    pf2 setup

--- Google Books API/Assets/Scripts/inputScript.cs
+++ Google Books API/Assets/Scripts/inputScript.cs
@@ -12,14 +12,29 @@ public class inputScript : MonoBehaviour
     {
         API = GameObject.Find("API");
         var textInput = GetComponent<InputField>();
-        var se = new InputField.SubmitEvent();
+      //  var se = new InputField.SubmitEvent();
         textInput.onEndEdit.AddListener(SubmitName);
     }
+    void Update()
+    {
+        if (Input.GetKeyDown("l"))
+        {
+            API.GetComponent<GoogleBooksAPI>().doSearch("Luca");
+            
+        }
+        else if (Input.GetKeyDown("o"))
+        {
+            API.GetComponent<GoogleBooksAPI>().doSearch("Oliver Wolter");
+        }
 
+        else if (Input.GetKeyDown("j"))
+        {
+            API.GetComponent<GoogleBooksAPI>().doSearch("Jake");
+        }
+    }
     public void SubmitName(string arg0)
     {
         // call method from API
-        API.GetComponent<GoogleBooksAPI>().doSearch(arg0);
-
+       
     }
 }
